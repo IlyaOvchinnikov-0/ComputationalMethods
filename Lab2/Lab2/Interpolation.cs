266c64b43bc024417919b2d0c80ad0da6495adee
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-05-10 18:27:33 +0400

    first commit

---
+++ Lab2/Lab2/Interpolation.cs
@@ -0,0 +1,53 @@
+﻿
+class Interpolation
+{
+    public static double LagrangePolynomial(double[] xA, double[] yA, double x)
+    {
+        double s = 0.0;
+
+        for (int i = 0; i < xA.Length; i++)
+        {
+            s += yA[i] * l(x, i, xA);
+        }
+
+        return s;
+    }
+
+    private static double l(double x, int i, double[] xA)
+    {
+        double numerator = 1.0, denominator = 1.0;
+        for (int j = 0; j < xA.Length; j++)
+        {
+            if (j == i) continue;
+            numerator *= x - xA[j];
+            denominator *= xA[i] - xA[j];
+        }
+        return numerator / denominator;
+    }
+
+    public static double[] ParabolicSpline(double[] xA, double[] yA, double x)
+    {
+        double[] s = new double[xA.Length - 1];
+
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            s[i] = a(xA, yA, i) * (x - xA[i]) * (x - xA[i]) + b(xA, yA, i) * (x - xA[i]) + yA[i];
+        }
+
+        return s;
+    }
+    private static double h(double[] xA, int i)
+    {
+        return xA[i+1] - xA[i];
+    }
+    private static double b(double[] xA, double[] yA, int i)
+    {
+        if (i == 0) return 0.0;
+        return ((2 * (yA[i] - yA[i - 1])) / h(xA, i)) - b(xA, yA, i - 1);
+    }
+
+    private static double a(double[] xA, double[] yA, int i)
+    {
+        return (b(xA, yA, i + 1) - b(xA, yA, i)) / (2 * h(xA, i));
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+var N = 23;
+var k = 4;
+var m = 10;
+
+double[] x = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 };
+double[] y = { 0.2*N, 0.3*m, 0.5*k, 0.6*N, 0.7*m, k, 0.8*N, 1.2*k, 1.3*m, N};
+
+
+//Console.WriteLine(Interpolation.LagrangePolynomial(new double[] { 1, 4, 5, 6 }, new double[] { 2, 3, 2, 3 }, 3.0));
+
+//Console.WriteLine(-5 + 9.7333*3 - 3*3*3 + 0.2667*3*3*3);
+
+Console.WriteLine(Interpolation.LagrangePolynomial(x, y, 3.0));
+
+Console.WriteLine();
+
+double[] s = Interpolation.ParabolicSpline(x, y, 3.0);
+
+foreach (double t in s)
+{
+    Console.WriteLine(t);
+
+}
+
