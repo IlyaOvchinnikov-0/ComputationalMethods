c4e4b385f49ef8a2b8fdc151b55a4dd9579f770c
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-05-10 19:42:17 +0400

    lab3

---
+++ Lab3/Lab3/NumericalIntegration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class NumericalIntegration
+    {
+        
+        public static float TrapezoidFormula(float a, float b, int m)
+        {
+            float h = (b - a) / m;
+
+            float sum = OriginalFunction(a) + OriginalFunction(b);
+
+            for (int i = 1; i <= m - 1; i++)
+            {
+                sum += 2 * OriginalFunction(a + i * h);
+            }
+
+            sum *= h / 2;
+
+            return sum;
+        }
+
+        private static float OriginalFunction(float x)
+        {
+            return (float)((0.1 + x * x * Math.Sin(x)) / (x * x - 5));
+        }
+
+        /*public static float GaussQuadratureFormula()
+        {
+
+        }*/
+    }
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,6 @@
+﻿using Lab3;
+
+float a = -0.2f, b = 0.4f;
+int m = 10;
+
+Console.WriteLine(NumericalIntegration.TrapezoidFormula(a, b, m));
